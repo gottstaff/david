cd6009b3c90897085eb8a8c7571a229e0f32c285
Gus <caocancio@gmail.com>
2020-07-05 03:13:56 +0200

    6

---
+++ data/traza_lidar_PNOA_11.js
@@ -0,0 +1,8 @@
+var json_traza_lidar_PNOA_11 = {
+"type": "FeatureCollection",
+"name": "traza_lidar_PNOA_11",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "local_idx": 1.0 }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -7.245321391813041, 42.427952583767045 ], [ -7.245178105807079, 42.427342511225504 ] ] ] } }
+]
+}
